b78e4b90791cb30fb1a3d7ad42da2cdc0d43b258
Cameron <85236636+Cswish24@users.noreply.github.com>
2022-03-01 19:19:30 -0600

    add postgres

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from flask import abort
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "S8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
